fcbaea7396d728e553e7ea94c13d2328f2bb4684
welchi <welchizm@gmail.com>
2019-10-31 17:22:51 +0900

    Update to reformat main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(GoodWelchi());
 
-class GoodWelchi extends StatelessWidget{
+class GoodWelchi extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:'GoodWelchi',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal
-      ),
-      home:HomePage()
-    );
+        title: 'GoodWelchi',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: HomePage());
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color(0xFFF8FBFF),
-          Color(0xFFFCFDFD)
-        ])
-      ),
+          gradient:
+              LinearGradient(colors: [Color(0xFFF8FBFF), Color(0xFFFCFDFD)])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
-          child:Column(
-            children: <Widget>[],
-          )
+            child: Column(
+          children: <Widget>[],
+        )),
       ),
-    ),
     );
   }
 }
